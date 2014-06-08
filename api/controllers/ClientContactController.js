90bb09437fa9c104e129dd1904f31cf06be5d486
root <davidw93.dw@gmail.com>
2014-06-08 18:44:27 +0000

    fixed spelling mistakes in model generation

---
+++ api/controllers/ClientContactController.js
@@ -0,0 +1,12 @@
+/*
+ *      Script Name:    ClientContactController.js
+ *      Status:         Not Implemented
+ *      Access URL:     <prefix>/ClientContact/
+ *      Script Usage:
+ *          Script is for CRUD operations on Client Contacts within the system
+ */
+
+var ClientContactController = {
+};
+
+module.exports = ClientContactController;
