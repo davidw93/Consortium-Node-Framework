aac4910669943bc7ce1a2a9e0db8442b1a085f28
Davey <davey.black@hotmail.com>
2014-06-10 10:26:19 +0100

    Documented ClientController.js

--- api/controllers/ClientController.js
+++ api/controllers/ClientController.js
@@ -7,19 +7,36 @@
  */
 
 var ClientController = {
+
+    /*
+     *      -- ACCESSIBLE BY BOARD --
+     *      create(req, res):
+     *          
+     *      Extracts the parameters required for a client and creates a new client object.
+     *      This function creates a standard client (only one kind in the system)
+     */
     create: function(req, res) {
         var cname = req.param("company_name");
         var website = req.param("website");
+        var address = req.param("address");
 
         Client.create({
             company_name: cname,
             website: website,
-            address: "testing because cba with that param"
+            address: address
         }).done(function(err, client) {
             if(err) return res.send(err, 500); //err 500 with the error for debugging
             return res.json(client); //return the client object as a JSON object
         });
     },
+
+    /*
+     *      -- ACCESSIBLE BY BOARD --
+     *      destroy(req, res):
+     *          
+     *      Extracts data to identify which client is to be destroyed,
+     *      This function destroys the client identified by the parameters passed to it
+     */
     destroy: function(req, res) {
         var cid = req.param("company_id");
         var company_name = req.param("company_name");
@@ -48,6 +65,12 @@ var ClientController = {
         return res.json({"msg": "failure, invalid paramaters to function"});
     },
 
+    /*
+     *      -- ACCESSIBLE BY ALL --
+     *      find(req, res):
+     *          
+     *      Will return all clients in the system meeting the requirements set by parameter(s)
+     */
     find: function(req, res){
         var clientID = req.param('id');
         var company_name = req.param('company_name');        
@@ -65,6 +88,12 @@ var ClientController = {
         }
     },
 
+    /*
+     *      -- ACCESSIBLE BY ALL --
+     *      findAll(req, res):
+     *          
+     *      Will return all clients in the system
+     */
     findAll: function(req, res){
 
         Client.find().done(function(err, clients){
