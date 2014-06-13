6073288568114ea233217f8fbe3c77b378d5722a
root <davidw93.dw@gmail.com>
2014-06-13 15:31:22 +0000

    updated user model without testing

--- assets/linker/js/models/user.js
+++ assets/linker/js/models/user.js
@@ -1,3 +1,10 @@
 ConsortiumNodeFramework.User = DS.Model.extend({
     fname: DS.attr(),
+    lname: DS.attr(),
+    DOB: DS.attr(),
+    pNumber: DS.attr(),
+    email: DS.attr(),
+    multiplier: DS.attr(),
+    accessLevel: DS.attr(),
+    password: DS.attr(),
 });
